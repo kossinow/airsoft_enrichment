ebf6a4f83b84c39cedae1c31e799c0c839ccf21f
Roman Kossinov <kossinow@gmail.com>
2026-04-28 19:27:35 +0500

    sound bug fix, refactoring

--- airsoft_enrichment.ino
+++ airsoft_enrichment.ino
@@ -1,6 +1,6 @@
 
-// пины кнопок
-#define btn_1_pin 2
+// пин кнопки
+#define btn_pin 2
 
 // пин геркона
 #define gerkon_pin 3
@@ -8,7 +8,7 @@
 // пин баззера
 #define buzzer_pin 4
 
-// пин баззера
+// пин релюшки
 #define relay_pin 5
 
 #define _LCD_TYPE 1 //Тип подключения дисплея: 1 - по шине I2C scl - a5, sdl - a4
@@ -21,7 +21,7 @@
 #include <EEPROM.h>
 
 LCD_1602_RUS lcd(0x27, 16, 2);
-Button btn_1(btn_1_pin);
+Button btn(btn_pin);
 Button gerkon(gerkon_pin);
 
 byte mode = 2; // флаг переключающий состояния
@@ -29,35 +29,32 @@ int timers[6] = {5, 10, 15, 30, 45, 60}; // таймеры в минутах
 int current_timer = 0; // текущее положение таймера
 int min;
 int sec = 0;
-bool trigger = true; // trigger for screen clearing
-long t_blink = 0; // counter for blinking
-bool t_flag = false; // flag for blinking
+bool trigger = true; // триггер для очищения экрана (костыль)
+long tick = 0; // таймер задержек
+bool blink = false; // флаг мигания
 
 void setup () {
-  Serial.begin(9600);
 
   // просто красивая инициализация экрана
   lcd.init();
-  lcd.backlight();
-  delay(300);
-  lcd.noBacklight();
-  delay(300);
   lcd.backlight();
   for (int i = 0; i < 2; i++) {
     for (int j = 0; j < 16; j++) {
       lcd.setCursor(j, i);
       lcd.write(255);
-      delay(25);
+      delay(20);
     }
   }
   lcd.clear();
-  delay(100);
-  if (!digitalRead(btn_1_pin)){
+  delay(50);
+
+  // вход в режим настроек
+  if (!digitalRead(btn_pin)){
     mode = 1;
   }
 
-  // подключаем кнопки
-  btn_1.begin();
+  // инициализация кнопки
+  btn.begin();
 
   // инициализация выводов
   pinMode(buzzer_pin, OUTPUT);
@@ -68,24 +65,16 @@ void setup () {
 void loop () {
 
   switch (mode) { // главное меню - переключение между состояниями
-    case 1: setting(); break;
-    case 2: waiting(); break;
-    case 3: counting(); break;
-    case 4: cocking(); break;
+    case 1: setting(); break; // настройка таймера
+    case 2: waiting(); break; // ожидание установки капсулы
+    case 3: counting(); break; // обратный отсчет
+    case 4: cocking(); break; // засвет капсулы
   }
 }
 
 void setting () {
 
-  if (btn_1.released()) {
-    current_timer ++ ;
-    if (current_timer > 5) current_timer = 0;
-    EEPROM.write(addr_current_timer, current_timer);
-    trigger = !trigger;
-    Serial.println(current_timer);
-  }
-
-  if (trigger) {
+  if (trigger) { // обновить информация на экране по флагу
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("УСТАНОВИ  ТАЙМЕР");
@@ -94,6 +83,14 @@ void setting () {
     lcd.setCursor(9, 1);
     lcd.print("мин");
     trigger = !trigger;
+  }
+
+  if (btn.pressed()) {
+    current_timer ++ ;
+    if (current_timer > 5) current_timer = 0;
+    EEPROM.write(addr_current_timer, current_timer);
+    trigger = true;
+    Serial.println(current_timer);
   }
 }
 
@@ -105,25 +102,31 @@ void waiting () {
     lcd.print("УСТАНОВИ КАПСУЛУ");
     lcd.setCursor(2, 1);
     lcd.print("ЧТОБЫ НАЧАТЬ");
-    trigger = false;
+    trigger = !trigger;
   }
 
-  if (gerkon.pressed()){
+  if (gerkon.pressed()){ // капсула установлена
     mode = 3;
     min = timers[EEPROM.read(addr_current_timer)];
     sec = 0;
     trigger = true;
+    tick = millis();
   } 
 
 }
 
 void counting (){
 
-  if (millis() - t_blink > 1000){ // обратный отсчет
-    t_blink = millis();
-    t_flag = !t_flag;
+  if (millis() - tick > 1000){ // обратный отсчет
+    tick = millis();
+    blink = !blink;
     sec --;
     trigger = true;
+    digitalWrite(buzzer_pin, 1);
+  }
+
+  if (millis() - tick > 1){ // выключаем звук
+    digitalWrite(buzzer_pin, 0);  
   }
 
   if (sec < 0){
@@ -134,9 +137,9 @@ void counting (){
   if (trigger) { // индикация
     lcd.clear();
     lcd.setCursor(6, 0);
-    if (min < 10) lcd.setCursor(7, 0); // для однозначних чисел
+    if (min < 10) lcd.setCursor(7, 0); // смещение для однозначних чисел
     lcd.print(min, DEC);
-    if (t_flag) {
+    if (blink) {
       lcd.setCursor(8, 0);
       lcd.write(58);
     }
@@ -155,20 +158,20 @@ void counting (){
       lcd.write(255);
     }
 
-    trigger = false;
+    trigger = !trigger;
   }
 
   if (min < 0){
     mode = 4;
     trigger = true;
-    t_blink = millis();
+    tick = millis();
   }
 
   if (gerkon.released()){ // отключение режима
     mode = 2;
     trigger = true;
+    digitalWrite(buzzer_pin, 0);
   } 
-
 }
 
 void cocking (){
@@ -179,10 +182,10 @@ void cocking (){
     lcd.print("КАПСУЛА");
     lcd.setCursor(4, 1);
     lcd.print("АКТИВНА");
-    trigger = false;
+    trigger = !trigger;
   }
 
-  if (t_blink - millis() < 3000) {
+  if (millis() - tick < 3000) { // звуковая индикация и засветка
     digitalWrite(buzzer_pin, 1);
     digitalWrite(relay_pin, 1);
   }
@@ -197,5 +200,4 @@ void cocking (){
     digitalWrite(buzzer_pin, 0);
     digitalWrite(relay_pin, 0);
   } 
-
 }
